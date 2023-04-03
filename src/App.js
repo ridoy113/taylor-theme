39b64b927c0568fc5bf2dd5f6d7c5f77d498ea75
ridoy113 <almamounhridoy113@gmail.com>
2023-04-03 23:58:08 +0600

    project making start

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
